b9477fd4bc0d2d3ddb14be3ad1ab185997ddada6
Ola Ishola <olaishola@hotmail.co.uk>
2023-01-27 11:40:17 +0100

    Update useFetch hook with error handling

--- src/components/Hooks/useFetch.js
+++ src/components/Hooks/useFetch.js
@@ -4,24 +4,29 @@ import React from 'react'
 const useFetch = (url) => {
   const [data, setData] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   const fetchData = React.useCallback(async (url) => {
     const response = await axios(url);
     return response.data;
   }, []);
 
-
   React.useEffect(() => {
     setLoading(true);
+
     fetchData(url)
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      })
       .then((data) => {
         setData(data);
         setLoading(false);
-      }
-      );
+
+      });
   }, [url, fetchData]);
 
-  return [data, loading];
+  return [data, loading, error];
 }
 
 export default useFetch
